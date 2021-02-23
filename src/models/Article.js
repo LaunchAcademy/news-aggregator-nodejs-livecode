f16d9c692d5e20496d4a2d233971fe7d1e24d1a2
Heidi Hood <heidih74@gmail.com>
2021-02-23 18:31:40 -0500

    final-code

--- src/models/Article.js
+++ src/models/Article.js
@@ -7,7 +7,35 @@ const articlesJson = () => {
 }
 
 class Article {
+  constructor({ title, url, description }) {
+    this.title = title
+    this.url = url
+    this.description = description
+  }
 
+  static findAll() {
+    const articlesData = articlesJson().articles
+    const articleObjects = articlesData.map((article) => {
+      return new Article(article)
+    })
+    return articleObjects
+  }
+
+  save() {
+    //retrieve all existing articles
+    // let articlesArray = articlesJson().articles
+    const articlesArray = this.constructor.findAll()
+
+    //add our new article to the existing articles
+    articlesArray.push(this)
+
+    //convert data to JSON and write to articles.json file
+    const jsonArticles = JSON.stringify({ articles: articlesArray })
+    fs.writeFileSync(articlesPath, jsonArticles)
+
+    // nice to have once we start adding in validations 
+    return true 
+  }
 }
 
 export default Article
--- src/routes/articlesRouter.js
+++ src/routes/articlesRouter.js
@@ -2,12 +2,29 @@ import express from "express"
 import fs from "fs"
 const articlesPath = "articles.json"
 
-// import Article from "../models/Article.js"
+import Article from "../models/Article.js"
 
 const articlesRouter = new express.Router()
 
-const articlesJson = () => {
-  return JSON.parse(fs.readFileSync(articlesPath))
-}
+articlesRouter.get("/", (req, res) => {
+  //return all of the article records from the JSON file as Article objects in an array and pass them to our views template 
+  //const articleObjects = Article.findAll()
+  res.render("articles/index", {articles: Article.findAll()})
+})
+
+articlesRouter.get("/new", (req, res) => {
+  res.render("articles/new")
+})
+
+articlesRouter.post("/", (req, res) => {
+  // const articleTitle = req.body.title
+  // const articleUrl = req.body.url
+  // const articleDescription = req.body.description
+  // const newArticle = new Article({title: articleTitle, url: articleUrl, description: articleDescription})
+  const formData = req.body 
+  const newArticle = new Article(formData)
+  newArticle.save()
+  res.redirect("/")
+})
 
 export default articlesRouter
