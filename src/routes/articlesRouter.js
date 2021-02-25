816f34820e2ad26112eb30984cbd8015f740f233
Heidi Hood <heidih74@gmail.com>
2021-02-25 16:18:57 -0500

    basic-validation

--- src/routes/articlesRouter.js
+++ src/routes/articlesRouter.js
@@ -16,15 +16,29 @@ articlesRouter.get("/new", (req, res) => {
   res.render("articles/new")
 })
 
+// articlesRouter.post("/", (req, res) => {
+//   // const articleTitle = req.body.title
+//   // const articleUrl = req.body.url
+//   // const articleDescription = req.body.description
+//   // const newArticle = new Article({title: articleTitle, url: articleUrl, description: articleDescription})
+//   const formData = req.body 
+//   const newArticle = new Article(formData)
+//   newArticle.save()
+//   res.redirect("/")
+// })
+
+//if the name of the article is not supplied when submitting the form, the user should not be redirected to /articles and instead they should remain on the form page
+
 articlesRouter.post("/", (req, res) => {
-  // const articleTitle = req.body.title
-  // const articleUrl = req.body.url
-  // const articleDescription = req.body.description
-  // const newArticle = new Article({title: articleTitle, url: articleUrl, description: articleDescription})
-  const formData = req.body 
-  const newArticle = new Article(formData)
-  newArticle.save()
-  res.redirect("/")
+  const formData = req.body
+  if (req.body.title.trim() === "") {
+    res.render("articles/new", {error: "title can't be blank"})
+  } else {
+    const newArticle = new Article(formData)
+    newArticle.save()
+    res.redirect("/")
+  }
 })
 
 export default articlesRouter
+
