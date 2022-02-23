8b55e17f37ca0640d43abddceec04162a650756f
Brianna Kincart <briannakincart@gmail.com>
2022-02-23 18:52:41 -0500

    final code

--- src/models/Article.js
+++ src/models/Article.js
@@ -1,13 +1,48 @@
 import fs from "fs"
 
+// path to your articles.json file
 const articlesPath = "articles.json"
 
-const articlesJson = () => {
-  return JSON.parse(fs.readFileSync(articlesPath))
-}
-
 class Article {
+  // We first need a constructor to set the structure for what an Article is
+  // Article takes in an object that includes a title, url, and description
+  constructor({ title, url, description }) {
+    this.title = title
+    this.url = url
+    this.description = description
+  }
 
+  // We want to call it like: Article.findAll
+  // We need a findAll method which is able to
+  static findAll() {
+    // read the JSON file
+    const fileData = fs.readFileSync(articlesPath)
+    // parse the data into a data structure
+    const parsedData = JSON.parse(fileData)
+    // drill down into the data to get the array of objects
+    const dataArray = parsedData.articles
+
+    // Turn it into Article objects
+    const articleObjects = dataArray.map((article) => {
+      return new Article(article)
+    })
+
+    // And spit out an array of Articles for us to use
+    return articleObjects
+  }
+
+  save() {
+    // get the existing list of articles
+    // const currentArticles = this.constructor.findAll()
+    const currentArticles = Article.findAll()
+    // add this new article to the existing list
+    console.log("this", this)
+    currentArticles.push(this)
+    // prepare the data to be written to the file
+    const fileData = JSON.stringify({ articles: currentArticles })
+    // write the data to the file
+    fs.writeFileSync(articlesPath, fileData)
+  }
 }
 
 export default Article
--- src/routes/articlesRouter.js
+++ src/routes/articlesRouter.js
@@ -1,13 +1,71 @@
 import express from "express"
-import fs from "fs"
-const articlesPath = "articles.json"
 
-// import Article from "../models/Article.js"
+import Article from "../models/Article.js"
 
 const articlesRouter = new express.Router()
 
-const articlesJson = () => {
-  return JSON.parse(fs.readFileSync(articlesPath))
-}
+articlesRouter.get("/", (req, res) => {
+  // res.send("Hi!")
+  // I should be able to see all the articles that have been submitted
+
+  // I need to get my data from the json file
+  // I'm going to use my Article model to get the data
+  // I'll have a `findAll` static method on my model
+  // (that I can call like `Article.findAll()`)
+  // which gives me back an array of Article objects
+  const articlesList = Article.findAll()
+
+  // I want to render the below view
+  // I need to pass the data from the model over to the view
+  res.render("articles/index", { articles: articlesList })
+  // res.render("articles/index", { articles: Article.findAll() })
+})
+
+articlesRouter.get("/new", (req, res) => {
+  res.render("articles/new", {
+    newArticle: new Article({
+      title: "",
+      url: "",
+      description: ""
+    })
+  })
+})
+
+articlesRouter.post("/", (req, res) => {
+  // When I successfully post an article, it should be saved to a JSON file via a POST request to /articles. I should then be redirected back to the "/articles" index page.
+  // If I fail to provide any of the required information, it does not alter my JSON file, and keeps me on the form page.
+  // capture the user's input from the form in variables
+  // const title = req.body.title
+  // const url = req.body.url
+  // const description = req.body.description
+  const { title, url, description } = req.body
+  // double-check to make sure they input everything we need
+  // if they gave all the necessary input
+  const newArticle = new Article(req.body)
+  if (title && url) {
+    // Save the article to the JSON file
+    // I want to use my model to do the logic work of adding the new article to the list
+    // and rewriting to the JSON file
+    // I'll create a `save()` instance method to do that work, and call it as `article.save()` here
+    // I'll first create an instance of an article with the user's input
+    // and then I'll try to `save()` it
+    // const newArticle = new Article({
+    //   title: title,
+    //   url: url,
+    //   description: description
+    // })
+    // const newArticle = new Article({ title, url, description })
+    newArticle.save()
+
+    // redirect them back to the "/articles" index page
+    res.redirect("/articles")
+  } else {
+    // otherwise
+    // don't touch the JSON file
+    // keep them on the form page
+    const error = "You did not provide all required information"
+    res.render("articles/new", { error, newArticle })
+  }
+})
 
 export default articlesRouter
