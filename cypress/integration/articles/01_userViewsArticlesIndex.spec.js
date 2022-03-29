db3abe1e7d54027104f517069751b5ee812b399a
ohad-porat <ohadp23@gmail.com>
2022-03-29 12:39:20 -0400

    add tests

--- cypress/integration/articles/01_userViewsArticlesIndex.spec.js
+++ cypress/integration/articles/01_userViewsArticlesIndex.spec.js
@@ -1,16 +1,30 @@
 /// <reference types="cypress" />
 
+import starterArticles from "../../fixtures/starterArticles.json"
+
 context("Articles Index", () => {
   beforeEach(() => {
     cy.visit("/articles")
   })
 
-  it("has a header displaying the word Articles!", () => {})
+  it("has a header displaying the word Articles", () => {
+    cy.get("h1").should("have.text", "Articles")
+  })
 
-  it("lists all articles", () => {})
+  it("lists all articles", () => {
+    cy.get("ul")
+      .find("li")
+      .first()
+      .should("have.text", `Title: ${starterArticles.articles[0].title}`)
+
+    cy.get("ul")
+      .find("li")
+      .last()
+      .should("have.text", starterArticles.articles[starterArticles.articles.length - 1].url)
+  })
 
   it("has a link to go to the new article form", () => {
-    cy.get('a').contains("Click here to add a new article!").click()
+    cy.get("a").contains("Click here to add a new article!").click()
     cy.url().should("include", "/articles/new")
   })
 })
--- cypress/integration/articles/02_userAddsANewArticle.spec.js
+++ cypress/integration/articles/02_userAddsANewArticle.spec.js
@@ -5,9 +5,27 @@ import starterArticles from "../../fixtures/starterArticles.json"
 const articlesFilePath = "articles.json"
 
 context("Articles New", () => {
-  it("has a header for the new article page", () => {})
+  beforeEach(() => {
+    cy.visit("/articles/new")
+  })
 
-  it("adds a travel attraction to the list upon submitting the form", () => {})
+  it("has a header for the new article page", () => {
+    cy.get("h1").should("have.text", "Add a new article!")
+  })
+
+  it("adds a travel attraction to the list upon submitting the form", () => {
+    cy.get("#title")
+      .type(newArticle.title)
+      .should("have.value", newArticle.title)
+
+    cy.get("#description")
+      .type(newArticle.description)
+      .should("have.value", newArticle.description)
+
+    cy.get("#url")
+      .type(newArticle.url)
+      .should("have.value", newArticle.url)
+  })
 
   it("has a link to go to the article index page", () => {
     cy.get('a').contains("Back to the homepage").click()
